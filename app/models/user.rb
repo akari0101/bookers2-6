459e73c6fe89ee15a666413f6647be918d4bde23
EC2 Default User <ec2-user@ip-172-31-43-203.ec2.internal>
2022-08-06 07:02:05 +0000

    Initial Commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,15 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
-  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :followings, through: :relationships, source: :followed
-  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :followers, through: :reverse_of_relationships, source: :follower
+  # フォローする側から中間テーブルへのアソシエーション
+  has_many :relationships, foreign_key: :following_id
+  # フォローする側からフォローされたユーザを取得する
+  has_many :followings, through: :relationships, source: :follower
+
+  # フォローされる側から中間テーブルへのアソシエーション
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: :follower_id
+  # フォローされる側からフォローしているユーザを取得する
+  has_many :followers, through: :reverse_of_relationships, source: :following
 
   validates :name, presence: true, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
@@ -23,17 +28,17 @@ class User < ApplicationRecord
   end
 
   def follow(user_id)
-    relationships.create(followed_id: user_id)
+    relationships.create(follower_id: user_id)
   end
 
   # ユーザーをアンフォローする
   def unfollow(user_id)
-    relationships.find_by(followed_id: user_id).destroy
+    relationships.find_by(follower_id: user_id).destroy
   end
 
   # 現在のユーザーがフォローしてたらtrueを返す
   def following?(user)
-    following.include?(user)
+    followings.include?(user)
   end
 
 end
